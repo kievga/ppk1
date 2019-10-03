093e38598cf65f86c7435ce5370ba080fcdd8d8e
kievga <tmcsaprillia@gmail.com>
2019-10-03 11:51:56 +0700

    Add files via upload

---
+++ ppk-uts-master/order-coffe/Customer.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace order_coffe
+{
+    partial class Customer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button_pesan = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.text_cari = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(252, 55);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(536, 333);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.ListView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nama Customer";
+            this.columnHeader2.Width = 102;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Jumlah Orang";
+            this.columnHeader5.Width = 79;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Tipe";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Total Pembayaran";
+            this.columnHeader3.Width = 157;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Status";
+            this.columnHeader4.Width = 129;
+            // 
+            // button_pesan
+            // 
+            this.button_pesan.Location = new System.Drawing.Point(713, 394);
+            this.button_pesan.Name = "button_pesan";
+            this.button_pesan.Size = new System.Drawing.Size(75, 23);
+            this.button_pesan.TabIndex = 1;
+            this.button_pesan.Text = "Pesan";
+            this.button_pesan.UseVisualStyleBackColor = true;
+            this.button_pesan.Click += new System.EventHandler(this.Button_pesan_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 164);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(99, 191);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nama Customer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Jumlah Orang";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 217);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Dine In";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(107, 218);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(79, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Take Away";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Tambah";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(90, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Edit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(171, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Hapus";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(343, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 43);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Welcome!";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Visible = false;
+            // 
+            // text_cari
+            // 
+            this.text_cari.Location = new System.Drawing.Point(9, 76);
+            this.text_cari.Name = "text_cari";
+            this.text_cari.Size = new System.Drawing.Size(148, 20);
+            this.text_cari.TabIndex = 17;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(9, 102);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 23);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "Cari Customer";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(99, 394);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "CheckOut";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Customer";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(55, 423);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Order List";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.text_cari);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_pesan);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Customer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customer";
+            this.Load += new System.EventHandler(this.Customer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button_pesan;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox text_cari;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ppk-uts-master/order-coffe/Customer.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace order_coffe
+{
+    public partial class Customer : Form
+    {
+        private static string connectionString = "server=localhost;port=3306;username=root;password=;database=seteguk;";
+        private MySqlConnection databaseConn = new MySqlConnection(connectionString);
+        public string id;
+
+        public Customer()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_pesan_Click(object sender, EventArgs e)
+        {
+            CheckOut Check = new CheckOut(id);
+            Check.Show();
+        }
+
+        public void refresh()
+        {
+            listView1.Items.Clear();
+            
+            string query = "SELECT * FROM stgk_pemesanan WHERE tanggal ='"+DateTime.Now.ToString("yyyy-MM-dd")+"' ";
+            MySqlCommand cmd = new MySqlCommand(query, databaseConn);
+            cmd.CommandTimeout = 60;
+            MySqlDataReader r = cmd.ExecuteReader();
+
+            if (r.HasRows)
+            {
+                while (r.Read())
+                {
+                    ListViewItem listcust = new ListViewItem(r["id_pemesanan"].ToString());
+                    listcust.SubItems.Add(r["nama_pembeli"].ToString());
+                    listcust.SubItems.Add(r["jumlah_orang"].ToString());
+                    listcust.SubItems.Add(r["type"].ToString());
+                    listcust.SubItems.Add(r["total_harga"].ToString());
+                    listcust.SubItems.Add(r["status"].ToString());
+                    listView1.Items.Add(listcust);
+                }
+                r.Close();
+            }
+            else
+            {
+                MessageBox.Show("Tidak ada data customer");
+            }
+        }
+
+        private void Customer_Load(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM stgk_pemesanan WHERE tanggal ='" + DateTime.Now.ToString("yyyy-MM-dd") + "' ";
+
+            databaseConn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, databaseConn);
+            cmd.CommandTimeout = 60;
+            MySqlDataReader r = cmd.ExecuteReader();
+
+            if (r.HasRows)
+            {
+                while (r.Read())
+                {
+                    ListViewItem listcust = new ListViewItem(r["id_pemesanan"].ToString());
+                    listcust.SubItems.Add(r["nama_pembeli"].ToString());
+                    listcust.SubItems.Add(r["jumlah_orang"].ToString());
+                    listcust.SubItems.Add(r["type"].ToString());
+                    listcust.SubItems.Add(r["total_harga"].ToString());
+                    listcust.SubItems.Add(r["status"].ToString());
+                    listView1.Items.Add(listcust);
+                }
+                r.Close();
+            }
+            else
+            {
+                MessageBox.Show("Tidak ada data customer");
+            }
+            databaseConn.Close();
+        }
+
+        ListViewItem cust;
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM stgk_pemesanan WHERE id_pemesanan = @id";
+
+            if (listView1.SelectedItems.Count > 0)
+            {
+                cust = listView1.SelectedItems[0];
+                string id = cust.SubItems[0].Text;
+
+                databaseConn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, databaseConn);
+                cmd.CommandTimeout = 60;
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                MySqlDataReader r = cmd.ExecuteReader();
+
+                if (r.HasRows)
+                {
+                    while (r.Read())
+                    {
+                        id = r["id_pemesanan"].ToString();
+                        textBox1.Text = r["nama_pembeli"].ToString();
+                        textBox2.Text = r["jumlah_orang"].ToString();
+                        if(r["type"].ToString() == "Dine In")
+                        {
+                            radioButton1.Checked = true;
+                        }
+                        else
+                        {
+                            radioButton2.Checked = true;
+                        }
+                        label4.Text = id;
+                    }
+                    r.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Tidak ada data pelanggan yang bisa dipilih");
+                }
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+            databaseConn.Close();
+        }
+
+        string tipe;
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO stgk_pemesanan (nama_pembeli, jumlah_orang, type, tanggal) VALUES (@nama, @jumlah, @type, NOW());";
+            if (radioButton1.Checked)
+            {
+                tipe = radioButton1.Text;
+            }
+            else
+            {
+                tipe = radioButton2.Text;
+            }
+            try
+            {
+                databaseConn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, databaseConn);
+                cmd.Parameters.AddWithValue("@nama", textBox1.Text);
+                cmd.Parameters.AddWithValue("@jumlah", textBox2.Text);
+                cmd.Parameters.AddWithValue("@type", tipe);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Berhasil menambahkan customer");
+                refresh();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                databaseConn.Close();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                text_cari.Text = "";
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE stgk_pemesanan SET nama_pembeli = @nama, jumlah_orang = @jumlah, type = @type WHERE id_pemesanan = @id;";
+            if (radioButton1.Checked)
+            {
+                tipe = radioButton1.Text;
+            }
+            else
+            {
+                tipe = radioButton2.Text;
+            }
+            string id = label4.Text;
+
+            try
+            {
+                databaseConn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, databaseConn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@nama", textBox1.Text);
+                cmd.Parameters.AddWithValue("@jumlah", textBox2.Text);
+                cmd.Parameters.AddWithValue("@type", tipe);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Berhasil mengubah customer");
+                refresh();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                databaseConn.Close();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                text_cari.Text = "";
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM stgk_pemesanan WHERE id_pemesanan = @id;";
+            string id = label4.Text;
+            try
+            {
+                databaseConn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, databaseConn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Berhasil menghapus customer");
+                refresh();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                databaseConn.Close();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                text_cari.Text = "";
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            Orderlist order = new Orderlist();
+            order.Show();
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+                string query = "SELECT * FROM stgk_pemesanan WHERE nama_pembeli LIKE '%" + text_cari.Text + "%' AND tanggal = '"+DateTime.Now.ToString("yyyy-MM-dd")+"';";
+            try
+            {
+                listView1.Items.Clear();
+                databaseConn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, databaseConn);
+                cmd.CommandTimeout = 60;
+                MySqlDataReader r = cmd.ExecuteReader();
+                if (r.HasRows)
+                {
+                    while (r.Read())
+                    {
+                        ListViewItem cust = new ListViewItem(r["id_pemesanan"].ToString());
+                        cust.SubItems.Add(r["nama_pembeli"].ToString());
+                        cust.SubItems.Add(r["jumlah_orang"].ToString());
+                        cust.SubItems.Add(r["type"].ToString());
+                        cust.SubItems.Add(r["total_harga"].ToString());
+                        cust.SubItems.Add(r["status"].ToString());
+                        listView1.Items.Add(cust);
+                    }
+                    r.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Data tidak ditemukan");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                databaseConn.Close();
+            }
+        }
+    }
+}
